4d664550efaba69f83688b19d789bc7b2fe01d0d
Vilson Jesuino <vilsonjesuino@gmail.com>
2023-07-25 22:47:40 -0300

    creating a microservice application with gateway

--- gateway/src/main/java/com/example/gateway/config/GatewayConf.java
+++ gateway/src/main/java/com/example/gateway/config/GatewayConf.java
@@ -13,7 +13,18 @@ public class GatewayConf {
         return builder.routes()
                 .route("client", r -> r
                         .path("/client")
-                        .uri("http://localhost:8082/"))
+                        .uri("http://localhost:8082"))
+                .route("staff", r -> r
+                        .path("/staff")
+                        .uri("http://localhost:8083"))
+                .route("cep - Brasilapi", r -> r
+                        .path("/cep/v1/**")
+                        .filters(f -> f.rewritePath("/cep/v1/(?<cep>.*)", "/api/cep/v1/${cep}"))
+                        .uri("https://brasilapi.com.br"))
+                .route("cep - viacep", r -> r
+                        .path("/cep/v2/**")
+                        .filters(f -> f.rewritePath("/cep/v2/(?<cep>.*)", "/ws/${cep}/json/"))
+                        .uri("https://viacep.com.br"))
                 .build();
     }
 }
